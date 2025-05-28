3573aacf9a158f5c381dce3bf9181cb627a10b34
ronen-aniti-projects <raniti@umd.edu>
2025-05-28 19:54:23 -0400

    Stop tracking build, install, log, and venv

--- ros2_ws/src/build/dynamic_obstacle_detector/build/lib/dynamic_obstacle_detector/main_node.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from rclpy.node import Node
-from cv_bridge import CVBridge 
-import cv2 
-from std_msgs.msg import Bool 
--- ros2_ws/src/build/dynamic_obstacle_detector/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ronen/Documents/Optical-Flow-Node/ros2_ws/src/install/dynamic_obstacle_detector'
